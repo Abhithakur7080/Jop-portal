5509e1fdc57077f128165e6460bf468242eb4c92
Abhijeet Kumar <abhijeetthakur7080@gmail.com>
2024-03-15 14:40:41 +0530

    firebase all functionaliy added with the sample hosting also

---
+++ public/js/auth.js
@@ -0,0 +1,168 @@
+const myModel = document.querySelectorAll(".modal");
+
+async function signup(e) {
+  e.preventDefault();
+  const email = document.querySelector("#signupEmail");
+  const password = document.querySelector("#signupPassword");
+  // console.log(email.value, password.value);
+  if(!email.value.length || !password.value.length){
+    return M.toast({ html: "Please enter all fields!", classes: "red" });
+  }
+  try {
+    const result = await firebase
+      .auth()
+      .createUserWithEmailAndPassword(email.value, password.value);
+      await result.user.updateProfile({
+        displayName: "Abhijeet"
+      })
+      // await result.user.sendEmailVerification()
+      CreateUserCollection(result.user)
+    M.toast({ html: `Welcome ${result.user.email}`, classes: "green" });
+    // console.log(result);
+  } catch (error) {
+    // console.error(error);
+    M.toast({ html: error.message, classes: "red" });
+  }
+  email.value = "";
+  password.value = "";
+  M.Modal.getInstance(myModel[0]).close();
+}
+
+async function login(e) {
+  e.preventDefault();
+  const email = document.querySelector("#loginEmail");
+  const password = document.querySelector("#loginPassword");
+  // console.log(email.value, password.value);
+  if(!email.value.length || !password.value.length){
+    return M.toast({ html: "Please enter all fields!", classes: "red" });
+  }
+  try {
+    const result = await firebase
+      .auth()
+      .signInWithEmailAndPassword(email.value, password.value);
+    M.toast({ html: `Welcome ${result.user.email}`, classes: "green" });
+    console.log(result);
+  } catch (error) {
+    if (error.code === "auth/invalid-credential") {
+      M.toast({ html: "Incorrect Email/Password", classes: "red" });
+    } else {
+      M.toast({ html: "Something Went Wrong!", classes: "red" });
+    }
+  }
+  email.value = "";
+  password.value = "";
+  M.Modal.getInstance(myModel[1]).close();
+}
+
+async function logout() {
+  try {
+    await firebase.auth().signOut();
+    document.querySelector('#profileImg').src = './user.png'
+    document.querySelector('#table').style.display = 'none'
+    M.toast({ html: `user logout successfully`, classes: "green" });
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+async function loginwithGoogle() {
+  try {
+    let provider = new firebase.auth.GoogleAuthProvider();
+    const result = await firebase.auth().signInWithPopup(provider);
+    CreateUserCollection(result.user)
+    M.Modal.getInstance(myModel[0]).close();
+    M.Modal.getInstance(myModel[1]).close();
+    console.log(result);
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+async function loginwithFacebook() {
+    try {
+        var provider = new firebase.auth.FacebookAuthProvider();
+        console.log(provider);
+      const result = await firebase.auth().signInWithPopup(provider);
+      console.log(result);
+      M.Modal.getInstance(myModel[0]).close();
+      M.Modal.getInstance(myModel[1]).close();
+      console.log(result);
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
+//if user already logged in then set the user
+const unsubscribe = firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    // console.log(user);
+    // getUserInfo(user.uid)
+    document.querySelector('#login').style.display = 'none'
+    document.querySelector('#signup').style.display = 'none'
+    document.querySelector('#logout').style.display = 'block'
+    getUserInfoRealTime(user.uid)
+    if(user.uid=='CgODvg15loXl217Y3nvpItZZeec2'){
+      getAllUserDetails()
+    }
+    console.log(user);
+  } else {
+    document.querySelector('#login').style.display = 'block'
+    document.querySelector('#signup').style.display = 'block'
+    document.querySelector('#logout').style.display = 'none'
+    getUserInfo(null)
+    console.log("Signout success");
+    M.toast({
+      html: `User not logged in please signup/register user`,
+      classes: "green",
+    });
+  }
+});
+//similar
+/*
+const user = firebase.auth().currentUser;
+if (user) {
+  console.log(user);
+} else {
+  console.log("Signout success");
+  M.toast({
+    html: `User not logged in please signup/register user`,
+    classes: "green",
+  });
+}
+*/
+
+// unsubscribe() // for code clean up
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  //not to be use
+  /*
+  rules_version = '2';
+
+service cloud.firestore {
+  match /databases/{database}/documents {
+    match /{document=**} {
+      allow read, write: if true;
+    }
+  }
+}
+   */
---
+++ public/js/firestore-db.js
@@ -0,0 +1,158 @@
+const userDetails = document.querySelector(".userDetails");
+const editUserProfile = document.querySelector("#edit-profile");
+
+async function CreateUserCollection(user) {
+  await firebase.firestore().collection("users").doc(user.uid).set({
+    uid: user.uid,
+    name: user.displayName,
+    email: user.email,
+    phone: "",
+    speciality: "",
+    portfolioUrl: "",
+    experience: "",
+  });
+}
+
+async function getUserInfo(userId) {
+  console.log(userId);
+  if (userId) {
+    const userInfoSnapshot = await firebase
+      .firestore()
+      .collection("users")
+      .doc(userId)
+      .get();
+    const userInfo = userInfoSnapshot.data();
+    console.log(userInfo);
+    if (userInfo) {
+      userDetails.innerHTML = `
+        <h3>${userInfo.name}</h3>
+        <h3>${userInfo.email}</h3>
+        <h3>${userInfo.phone}</h3>
+    `;
+    }
+  } else {
+    userDetails.innerHTML = `
+    <h3>please login</h3>
+`;
+  }
+}
+
+function getUserInfoRealTime(userId) {
+  if (userId) {
+    const userInfoDocRef = firebase.firestore().collection("users").doc(userId);
+    userInfoDocRef.onSnapshot((doc) => {
+      if (doc.exists) {
+        const userInfo = doc.data();
+        console.log(userInfo);
+        if (userInfo) {
+          userDetails.innerHTML = `
+          <ul class="collection">
+      <li class="collection-item"><h1>${userInfo.name}</h1></li>
+      <li class="collection-item">${userInfo.email}</li>
+      <li class="collection-item">${userInfo.phone}</li>
+      <li class="collection-item">${userInfo.speciality}</li>
+      <li class="collection-item">${userInfo.experience}</li>
+    </ul>
+    <a href="${userInfo.portfolioUrl}" class="btn" target="_blank">visit my portfolio</a>
+        <button class="btn waves-effect #283593 indigo darken-3 modal-trigger" href="#modal3">Edit profile</button>
+    `;
+          editUserProfile["name"].value = userInfo.name;
+          editUserProfile["profileEmail"].value = userInfo.email;
+          editUserProfile["phone"].value = userInfo.phone;
+          editUserProfile["speciality"].value = userInfo.speciality;
+          editUserProfile["portfolioUrl"].value = userInfo.portfolioUrl;
+          editUserProfile["experience"].value = userInfo.experience;
+          if (firebase.auth().currentUser.photoURL) {
+            document.querySelector("#profileImg").src =
+              firebase.auth().currentUser.photoURL;
+            console.log(document.querySelector("#profileImg").src);
+          }
+        }
+      }
+    });
+  } else {
+    userDetails.innerHTML = `
+    <h3>please login</h3>
+`;
+  }
+}
+async function updateUserProfile(e) {
+  e.preventDefault();
+  const userDocRef = await firebase
+    .firestore()
+    .collection("users")
+    .doc(firebase.auth().currentUser.uid);
+  userDocRef.update({
+    name: editUserProfile["name"].value,
+    email: editUserProfile["profileEmail"].value,
+    phone: editUserProfile["phone"].value,
+    speciality: editUserProfile["speciality"].value,
+    portfolioUrl: editUserProfile["portfolioUrl"].value,
+    experience: editUserProfile["experience"].value,
+  });
+  M.Modal.getInstance(myModel[2]).close();
+}
+
+function uploadImage(e) {
+  const uid = firebase.auth().currentUser.uid;
+  const filesRef = firebase.storage().ref().child(`/users/${uid}/profile`);
+  const uploadTask = filesRef.put(e.target.files[0]);
+
+  uploadTask.on(
+    "state_changed",
+    (snapshot) => {
+      let progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      console.log("Upload is " + progress + "% done");
+      if (progress === 100) {
+        // Upload completed, get the download URL
+        alert("uploaded");
+      }
+    },
+    (error) => {
+      // Handle unsuccessful uploads
+      console.log(error);
+    },
+    () => {
+      uploadTask.snapshot.ref.getDownloadURL().then((downloadURL) => {
+        console.log("File available at", downloadURL);
+        document.querySelector("#profileImg").src = downloadURL;
+        firebase
+          .auth()
+          .currentUser.updateProfile({
+            photoURL: downloadURL,
+          })
+          .then(() => {
+            console.log("User profile updated with photoURL");
+          })
+          .catch((error) => {
+            console.error("Error updating user profile:", error);
+          });
+        // alert("Profile is successfully uploaded");
+      });
+    }
+  );
+}
+
+function getAllUserDetails(){
+  document.querySelector('#table').style.display = 'table'
+  const usersRef = firebase.firestore().collection('users')
+  usersRef.onSnapshot(querySnap => {
+    // console.log(querySnap);
+    querySnap.docs.forEach(doc => {
+      const info = doc.data()
+      // console.log(info);
+      if(firebase.auth().currentUser.uid !== info.uid){
+        document.querySelector('#tbody').innerHTML += `
+      <tr>
+        <td>${info.name}</td>
+        <td>${info.email}</td>
+        <td>${info.phone}</td>
+        <td>${info.speciality}</td>
+        <td>${info.experience}</td>
+        <td><a href="${info.portfolioUrl}" class="btn" target="_blank">visit</a></td>
+      </tr>
+      `
+      }
+    })
+  })
+}
